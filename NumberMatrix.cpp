cd17749ed5cbaf3a6358ea18ed04d69044b474d3
HiếuIT <87519700+hieuitest@users.noreply.github.com>
2023-02-19 14:18:33 +0700

    Add files via upload

---
+++ NumberMatrix.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for ( int i = 0 ; i < n ; i++)
+    {
+        for ( int j = i + 1 ; j <= n ; j++)
+        {
+            cout << j << " " ;
+            if (j == n && i > 0 )
+            {
+                for (int k = 1 ; k <= i ; k++)
+                {
+                    cout << k << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
